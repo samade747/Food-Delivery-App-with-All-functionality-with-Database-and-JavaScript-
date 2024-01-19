d15597601fa3f7e3b6fbcb83e34e956643e2e467
samade747 <samad.e747@gmail.com>
2024-01-19 10:27:53 +0500

    1

---
+++ login/app.js
@@ -0,0 +1,11 @@
+import { signUp, login, addInDBById, getLoggedInUser, getData, updateData, uploadFile, getAllDataOrderedByTimestamp, logout, addInDB, deletData } from "./utilities/functions.mjs"; 
+
+const loginBtn = document.getElementById('loginBtn')
+
+const login = () => {
+    
+
+
+}
+
+loginBtn && loginBtn.addEventListener('click', login)
